2bf1c2a82130976bbdb9e65dca4a44929fabdc71
DapperZeus <33703306+DapperZeus@users.noreply.github.com>
2017-12-21 14:45:17 -0700

    updated master

---
+++ ContactList/ContactTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Contactlist
+{
+    public partial class ContactTableViewController : UITableViewController
+    {
+        public ContactTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
