ea50338f38be848a20a8ee4ee93723a2a10d5ded
DapperZeus <33703306+DapperZeus@users.noreply.github.com>
2018-01-08 15:02:51 -0700

    updated master

---
+++ ContactList/DetailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Contactlist
+{
+    public partial class DetailsViewController : UIViewController
+    {
+        public DetailsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
